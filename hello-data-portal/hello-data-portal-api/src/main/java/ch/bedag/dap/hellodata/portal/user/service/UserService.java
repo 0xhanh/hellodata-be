a5eadbae12c87d4a74a3f8f72072cc60b89f1519
hanhvd <hanhvd@gmail.com>
2025-07-23 14:37:53 +0700

    fix conflict code

--- hello-data-portal/hello-data-portal-api/src/main/java/ch/bedag/dap/hellodata/portal/user/service/UserService.java
+++ hello-data-portal/hello-data-portal-api/src/main/java/ch/bedag/dap/hellodata/portal/user/service/UserService.java
@@ -403,17 +403,18 @@ public class UserService {
     }
 
     @Transactional
-    public void updateContextRolesForUser(UUID userId, UpdateContextRolesForUserDto updateContextRolesForUserDto) {
+    public void updateContextRolesForUser(UUID userId, UpdateContextRolesForUserDto updateContextRolesForUserDto, boolean sendBackUserList) {
         updateContextRoles(userId, updateContextRolesForUserDto);
         synchronizeDashboardsForUser(userId, updateContextRolesForUserDto.getSelectedDashboardsForUser());
         UserEntity userEntity = getUserEntity(userId);
-        synchronizeContextRolesWithSubsystems(userEntity, updateContextRolesForUserDto.getContextToModuleRoleNamesMap());
+        synchronizeContextRolesWithSubsystems(userEntity, sendBackUserList, updateContextRolesForUserDto.getContextToModuleRoleNamesMap());
         // hvd
         try {
             notifyUserViaEmail(userId, updateContextRolesForUserDto);
         } catch (Exception e) {
             log.error("Error while notifying user via email", e);
         }
+        notifyUserViaEmail(userId, updateContextRolesForUserDto);
     }
 
     @Transactional(readOnly = true)
